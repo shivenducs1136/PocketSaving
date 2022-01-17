27d01b58869cd3c39be8a60cdc12e6b262b40384
ananyapunia28 <ananyapunia12@gmail.com>
2022-01-17 12:17:52 +0530

    Bottom Nav

--- app/src/main/java/com/dsckiet/pocketsaving/MainActivity.kt
+++ app/src/main/java/com/dsckiet/pocketsaving/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import androidx.core.content.ContentProviderCompat.requireContext
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
@@ -13,15 +12,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.logoutbtn)
-        button.setOnClickListener {
-            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
-            val googleSignInClient = GoogleSignIn.getClient(this, gso)
-            googleSignInClient.signOut()
-            Toast.makeText(this, "Signout Successfully", Toast.LENGTH_SHORT).show()
-            val i = Intent(this, SplashActivity::class.java)
-            startActivity(i)
-            finish()
-        }
     }
 }
--- app/src/main/java/com/dsckiet/pocketsaving/ui/PayOnlineFragment.kt
+++ app/src/main/java/com/dsckiet/pocketsaving/ui/PayOnlineFragment.kt
@@ -1,10 +1,11 @@
-package com.dsckiet.pocketsaving
+package com.dsckiet.pocketsaving.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dsckiet.pocketsaving.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
