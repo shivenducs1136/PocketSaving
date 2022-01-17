704be6c1029ceddb01b01cf1ffc98b98d61379ee
ananyapunia28 <ananyapunia12@gmail.com>
2022-01-18 00:33:59 +0530

    Profile authenticated

---
+++ app/src/main/java/com/dsckiet/pocketsaving/dataclass/User.kt
@@ -0,0 +1,9 @@
+package com.dsckiet.pocketsaving.dataclass
+
+data class User(
+    val DisplayName : String? = null,
+    val FamilyName : String? = null ,
+    val Email :String? = null ,
+    val PhotoURL : String? = null ,
+    val Account : String? = null ){
+}
--- app/src/main/java/com/dsckiet/pocketsaving/ui/ProfileFragment.kt
+++ app/src/main/java/com/dsckiet/pocketsaving/ui/ProfileFragment.kt
@@ -5,16 +5,25 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import androidx.databinding.DataBindingUtil
 import android.view.ViewGroup
 import android.widget.Toast
 import com.dsckiet.pocketsaving.R
 import com.dsckiet.pocketsaving.SplashActivity
+import com.dsckiet.pocketsaving.databinding.FragmentProfileBinding
+import com.dsckiet.pocketsaving.dataclass.User
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 
 class ProfileFragment : Fragment() {
+    private lateinit var binding : FragmentProfileBinding
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +40,19 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var signInAccount: GoogleSignInAccount? = GoogleSignIn.getLastSignedInAccount(this.requireContext())
+        if(signInAccount!=null){
+            binding.name.text = signInAccount.displayName
+            Picasso.with(context)
+                .load(signInAccount.photoUrl)
+                .into(binding.ppic)
+            binding.email.text = signInAccount.email
+
+            databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+            val User = User(signInAccount.displayName,signInAccount.familyName,signInAccount.email,signInAccount.photoUrl.toString(),signInAccount.account.toString())
+            signInAccount.displayName?.let { databaseReference.child(it).setValue(User) }
+        }
+
         logoutBtn.setOnClickListener {
             val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
             val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
