b6e14b12e0d9f2424f994a88b95149489d693b27
ananyapunia28 <ananyapunia12@gmail.com>
2022-01-17 23:02:33 +0530

    Profile added

--- app/src/main/java/com/dsckiet/pocketsaving/MainActivity.kt
+++ app/src/main/java/com/dsckiet/pocketsaving/MainActivity.kt
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener {
+            navController.navigate(R.id.floatingBtnFragment)
+        }
+        val plan = findViewById<Button>(R.id.plan)
+        plan.setOnClickListener {
+            navController.navigate(R.id.planPaymentsFragment)
+        }
     }
 }
